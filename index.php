e5151e31341858c6c3d90419befec015ad366a3f
Jyles Coad-Ward <26735262+jylescoad-ward@users.noreply.github.com>
2019-10-03 23:00:28 +0800

    quickie fix

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 		$statdir = $installdir + "/modules/stats/";
 
 			//Getting contents of package.json AND parsing it.
-	$packageJSON = json_decode(file_get_contents($installdir."/package.json");, true);
+	$packageJSON = json_decode(file_get_contents($installdir."/package.json"), true);
 
 
 			//Checks what the user requested to our responses.
