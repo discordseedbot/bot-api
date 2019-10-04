09682bfa03bc6d46d8f70341bb9658dd1f505102
Jyles Coad-Ward <26735262+jylescoad-ward@users.noreply.github.com>
2019-10-04 13:00:53 +0800

    Fixed and Made file smaller and easy to understand.

--- index.php
+++ index.php
@@ -8,77 +8,40 @@
 
 
 			//Checks if ?req is in the URL bar after the file location
-	if (isset($_GET[['req']])){$request = $GET_['req'];}else{header("HTTP/1.1 400 Bad Request");}
+	$req = $_GET['req'];
 	
 			//Making stuff easy for my
 		$installdir = $SeedBotInstallLocation;
-		$statdir = $installdir + "/modules/stats/";
+		$statdir = $installdir."/modules/stats";
 
 			//Getting contents of package.json AND parsing it.
-	$packageJSON = json_decode(file_get_contents($installdir."/package.json"), true);
+	$packageJSON = json_decode(file_get_contents($installdir."\package.json"), true);
 
+			//Debugging Stuff
+		//var_dump($_GET);
+		//var_dump($packageJSON);
 
 			//Checks what the user requested to our responses.
-	switch ($request) {
-		case "usrcount":
-			echo file_get_contents($statdir + "users.txt");
-			break;
-		case "channelcount":
-			echo file_get_contents($statdir + "channels.txt");
-			break;
-		case "guildcount":
-			echo file_get_contents($statdir + "guilds.txt");
-			break;
-		case "branch":
-			break;
-			echo $packageJSON["branch"];
-		case "packageName":
-			echo $packageJSON['name'];
-			break;
-		case "botversion":
-			echo $packageJSON['version'];
-			break;
-		case "build":
-			echo $packageJSON["build"];
-			break;
-		case "description":
-			echo $packageJSON["description"];
-			break;	
-		case "botOwnerID":
-			echo $packageJSON["ownerID"];
-			break;
-		case "engines":
-			echo $packageJSON["engines"];
-			break;
-		case "packageScripts":
-			echo $packageJSON['scripts'];
-			break;
-		case "botDependencies":
-			echo $packageJSON['dependencies'];
-			break;
-		case "packageRepository":
-			echo $packageJSON['repository'];
-			break;
-		case "author":
-			echo $packageJSON["author"];
-			break;
-		case "license":
-			echo $packageJSON["license"];
-			break;
-		case "bugReportURL":
-			echo $packageJSON['bugs']['url'];
-			break;
-		case "botHomepage":
-			echo $packageJSON['homepage'];
-			break;
 
-			//If the user made an invalid request
-			//e.g ?req     ?req=lolxd
-
-		default:
-			echo("HTTP/1.1 400 Bad Request");
-			break;
-	}
+	if ($req === "test"){					echo "<h1>It Works!</h1>";}
+	elseif ($req === "usercount"){			echo file_get_contents($statdir."users.txt");}
+	elseif ($req === "channelcount"){		echo file_get_contents($statdir."channels.txt");}
+	elseif ($req === "guildcount"){			echo file_get_contents($statdir."guilds.txt");}
+	elseif ($req === "botBranch"){			echo $packageJSON["branch"];}
+	elseif ($req === "packageName"){		echo $packageJSON['name'];}
+	elseif ($req === "botVersion"){			echo $packageJSON['version'];}
+	elseif ($req === "botBuild"){			echo $packageJSON["build"];}
+	elseif ($req === "botDescription"){		echo $packageJSON["description"];}
+	elseif ($req === "botOwnerID"){			echo $packageJSON["ownerID"];}
+	elseif ($req === "botEngines"){			echo $packageJSON["engines"];}
+	elseif ($req === "packageScripts"){		echo $packageJSON['scripts'];}
+	elseif ($req === "botDependencies"){	echo $packageJSON['dependencies'];}
+	elseif ($req === "packageRepository"){	echo $packageJSON['repository'];}
+	elseif ($req === "author"){				echo $packageJSON["author"];}
+	elseif ($req === "botLicense"){			echo $packageJSON["license"];}
+	elseif ($req === "botBugReportUrl"){	echo $packageJSON['bugs']['url'];}
+	elseif ($req === "botHomepage"){		echo $packageJSON['homepage'];}
+	else{									echo "400 Bad Request<br>Please look at the source code of the API to grasp a better understanding!<br>";}
 
 
 ?>
