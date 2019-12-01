9d1c9f245dfa1c9feb9d1b134bdce6b08ed3a837
jylescoad-ward <jylescoad-ward@DESKTOP.local>
2019-12-01 20:33:28 +0800

    Added Log Capabilities and New Log Location.

--- index.php
+++ index.php
@@ -15,12 +15,32 @@
 	$apiVersion = "1.2.3";
 	$apiLicense = "GPL-3.0-or-later";
 
-	include './functions/get.php';
-	include './functions/update.php';
+	Include './functions/get.php';
+	Include './functions/update.php';
+	Include "./functions/ip.php";
+
+
+ /* Log users */
+    $writeDirectory = "/etc/darioxlog";
+    $logFileName = "seedbot-api.csv";
+    $logWriteDestination = $writeDirectory."/".$logFileName;
+    $log = fopen($logWriteDestination, a);
+    $writeToLogType;
+    $writeToLogIP = $_SERVER['REMOTE_ADDR'];
+    $writeToLogUserAgent = str_replace(",", " | ", $_SERVER['HTTP_USER_AGENT']);
+    $writeToLogHostname = $_SERVER['REMOTE_HOST'];
+    $writeToLogCountry = ip_info($_SERVER['REMOTE_ADDR'], "country");
+    $writeToLogTime = date('l j \of F Y h;i:s A');
+    $writeToLogReferer = $_SERVER['HTTP_REFERER'];
+
+
+
+
 
 			//Checks what the user requested to our responses.
-
+    $logType = "Invalid Request/Inproper Use.";
 	if(!isset($_GET['token'])){
+    	$logType = "Request";
 		if ($req === "connectionTest"){				echo "true";}
 		elseif ($req === "userCount"){				getData($configLocation,$req);}
 		elseif ($req === "channelCount"){			getData($configLocation,$req);}
@@ -45,10 +65,20 @@
 		//Checks if token given is valid
 		if (!strpos($token, file_get_contents($tokenLocation))){
 			//Runs updateData from update.php
+			$tokenValid = true;
 			updateData($configLocation, $req, $data);
+			$logType = "Push";
 		} else {
 			echo "4xx Bad Token<br>You have sent an invalid token, either you bot is not configured properly or you are not authorized to do so.";
+			$tokenValid = false;
+			$logType = "Failed Push"
 		}
 	}
+
+	$writeToLog = $writeToLogTime.",".$writeToLogIP.",".$writeToLogUserAgent.",".$writeToLogCountry.",".str_replace(",", " ", $req).",".$logType.",".$writeToLogReferer.",".$writeToLogHostname."\n";
+	fwrite($log, $writeToLog);
+    fclose($log);
+
+
 
 ?>
