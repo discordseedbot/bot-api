e98078d8da4c2f7f85d7fdf82dbda0a52cf79d77
Jyles Coad-Ward <jyles@dariox.club>
2020-06-10 10:36:30 +0800

    Removed Useless Variables and Stripped Useless Things.

--- functions/errorHandle.php
+++ functions/errorHandle.php
@@ -1,34 +1,26 @@
 <?php
 
 	require_once(__DIR__."/json.php");
-function errorHandle($domain,$reason){
-
-	if (strpos("\\",__DIR__)) {
-		$rootDir = str_replace("functions","",__DIR__);
-		$funcDir = __DIR__."\\";
-		$config = json_decode(file_get_contents($rootDir."config.json"));
-		$dataLoc = $rootDir.$config->dataLoc."\\";
-		$errorLoc = $dataLoc."errors.json";
-	} else {
+	function errorHandle($domain,$reason){
 		$rootDir = str_replace("functions","",__DIR__);
 		$funcDir = __DIR__."/";
 		$config = json_decode(file_get_contents($rootDir."config.json"));
-		$dataLoc = $rootDir.$config->dataLoc."/";
-		$errorLoc = $dataLoc."errors.json";
-	}
-	$error = json_decode(file_get_contents($errorLoc),true);
-	// Check if domain exists
-	if (!is_valid_json(json_encode($error[$domain]))) {
-		echo str_replace("\\/","/",json_bute($error["developer"]["unknownDomain"]));
-		return $error["developer"]["unknownDomain"];
-	}
-	// Check of response exists
-	if (!is_valid_json(json_encode($error[$domain][$reason]))) {
-		echo str_replace("\\/","/",json_bute($error["developer"]["unknownReason"]));
-		return $error["developer"]["unknownReason"];
-	}
+		$dataLoc =$rootDir.$config->dataLoc."/";$dataLoc."errors.json"
 
-	echo str_replace("\\/","/",json_encode($error[$domain][$reason]));
-	return $error[$domain][$reason];
-}
+
+		$error = json_decode(file_get_contents($dataLoc."errors.json"),true);
+		// Check if domain exists
+		if (!is_valid_json(json_encode($error[$domain]))) {
+			echo str_replace("\\/","/",json_bute($error["developer"]["unknownDomain"]));
+			return $error["developer"]["unknownDomain"];
+		}
+		// Check of response exists
+		if (!is_valid_json(json_encode($error[$domain][$reason]))) {
+			echo str_replace("\\/","/",json_bute($error["developer"]["unknownReason"]));
+			return $error["developer"]["unknownReason"];
+		}
+
+		echo str_replace("\\/","/",json_encode($error[$domain][$reason]));
+		return $error[$domain][$reason];
+	}
  ?>
